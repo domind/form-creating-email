4ffc80a9232d1f1422ebcaa6bcc0620afb5483d6
domind <domind1@gmail.com>
2019-11-25 21:21:45 +0200

    Updates

--- src/dateconversion.js
+++
@@ -1,7 +0,0 @@
-export function convertDate(myDate) {
-  console.log(myDate)
-  const current_myDatetime = new Date(myDate)
-  return (current_myDatetime.getDate()<10 ?"0" +current_myDatetime.getDate():current_myDatetime.getDate() )+ "-" +
-    ((current_myDatetime.getMonth() + 1 < 10) ? "0" + (parseInt(current_myDatetime.getMonth())+1) : (current_myDatetime.getMonth())+1) + "-" +
-    current_myDatetime.getFullYear()
-}
